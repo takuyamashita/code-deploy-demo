178f340da6af5015e64e00dee51d65ee8cf02e15
takuyamashita <57676798+takuyamashita@users.noreply.github.com>
2023-10-01 19:38:40 +0900

    test

--- src/gin/main.go
+++ src/gin/main.go
@@ -17,7 +17,6 @@ var logger *zap.Logger
 func main() {
 
 	currentDir, _ := os.Getwd()
-
 	_, err := os.OpenFile(fmt.Sprintf("%s/%s", currentDir, "gin.log"), os.O_RDWR|os.O_CREATE|os.O_APPEND, 0666)
 	if err != nil {
 		log.Fatal(err)
